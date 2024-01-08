0f5c1baa6248af9263ea059d1a16007355969abf
bunny <kawaiiitzuyu@gmail.com>
2024-01-08 14:03:44 +0000

    graphing stuff

--- partavgfiles/graphing.py
+++ partavgfiles/graphing.py
@@ -8,22 +8,24 @@ with open("/Users/bunny/Documents/msci/mscigit/partavgfiles/partavg.dat") as fil
 
 print(np.shape(array))
 
-t = np.linspace(0,1,101)
-arr = np.zeros(10)
-for x in range(10):
+t = np.linspace(0,1,4001)
+arr = np.zeros(11)
+for x in range(11):
     y = linregress(t,array[x,:])
     arr[x] = y.slope
 
-
+t0 = [0,0.005,0.01,0.025,0.05,0.075,0.1,0.25,0.5,0.75,1]
 
 plt.figure()
-
-plt.plot(arr)
+plt.xlabel('Gamma')
+plt.ylabel('Drift Velocity')
+plt.plot(t0,arr,)
 
 plt.show()
 
-# plt.figure()
-# for x in range(10):
-#     plt.plot(t,array[x,:])
-
-# plt.show()
+plt.figure()
+plt.xlabel('Westward Drift')
+plt.ylabel('Time')
+for x in range(11):
+    plt.plot(array[x,:],t)
+plt.show()
