2e6ebe2c4fe5c2e3845c775c184cf6d7a4e97a2b
bunny <kawaiiitzuyu@gmail.com>
2024-01-13 15:33:40 +0000

    plots fully done kinda

--- partavgfiles/graphing.py
+++ partavgfiles/graphing.py
@@ -8,7 +8,7 @@ with open("/Users/bunny/Documents/msci/mscigit/partavgfiles/partavg.dat") as fil
 
 print(np.shape(array))
 array = array*400
-t = np.linspace(0,4000*0.25,4001)
+t = np.linspace(0,4000*0.6,4001)
 arr = np.zeros(11)
 for x in range(11):
     y = linregress(t,array[x,:])
@@ -17,15 +17,15 @@ for x in range(11):
 t0 = [0.1,0.5,1,2,3,4,9,25,36,49,64]
 
 fig1 = plt.figure()
-plt.xlabel('Amplitude scaling')
-plt.ylabel('Drift Velocity km/ time')
+plt.xlabel('Amplitude scaling ($alpha$)')
+plt.ylabel('Drift Velocity / $\mathrm{kmday}^{-1}$')
 plt.plot(t0,arr,)
 
 plt.show()
 
 fig2 = plt.figure()
 plt.xlabel('Westward Drift / km')
-plt.ylabel('Time')
+plt.ylabel('Time / day')
 for x in range(11):
     plt.plot(array[x,:],t,label=str(t0[x]))
 
