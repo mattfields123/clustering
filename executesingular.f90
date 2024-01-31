8d6b83cdc9d062a2384e4ce149ae50a5140f7a89
bunny <kawaiiitzuyu@gmail.com>
2024-01-31 01:20:44 +0000

    idk but like consistent

---
+++ executesingular.f90
@@ -0,0 +1,29 @@
+program executearray
+use passivetracers
+use constants
+use parameters
+implicit none
+real(dp) :: phase1(65,65), phase2(65,65), time1(65,65), time2(65,65)
+real(dp) :: g
+
+phase1 = tau*random_matrix(65,65)
+phase2 = tau*random_matrix(65,65)
+time1 = vu*random_matrix(65,65)
+time2 = vu*random_matrix(65,65)
+g = 0.1
+
+
+open(1,file='partavg.dat')
+
+
+write(1,*) vel_passive(time_steps, phase1, phase2, time1, time2, N_particles , g)
+
+
+
+close(1)
+
+!print *, char(7)  
+!call execute_command_line ("powershell.exe -File ../email.ps1", exitstat=i)
+!print *, "Exit status of external_prog.exe was ", i
+
+end program executearray
