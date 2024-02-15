c0ac8107c773341e9f6b22e6d69bee469d65eab4
bunny <kawaiiitzuyu@gmail.com>
2024-02-15 16:17:06 +0000

    added velocitytesting

--- velocitytest.f90
+++ velocitytest.f90
@@ -7,6 +7,10 @@ use velocity
 
 integer :: tsteps, meshsize
 
+meshsize = 10
+tsteps = 10
+
+call velocitycomp(meshsize,tsteps)
 
 
 
@@ -17,29 +21,36 @@ integer :: tsteps, meshsize
 
 contains 
 
-function velocitycomp(meshsize,tsteps) return done
+subroutine velocitycomp(meshsize,tsteps)
 integer :: done
 integer :: tsteps, meshsize
 real(dp), allocatable :: t_array(:)
 real(dp), allocatable :: x_array(:), y_array(:)
-real(dp) :: vel(2), t, x, y, time1(65,65), time2(65,65), phase1(65,65), phase2(65,65)
+real(dp) :: vel(2), t, x, y, time1(65,65), time2(65,65), phase1(65,65), phase2(65,65), dispersions(65,65), amplitudes(65,65)
 integer :: t_c, x_c, y_c
-real(dp) :: dt = 0.25
+real(dp) :: dt = 0.25,g=0.
 
 
 
-t_array = linspace(0.,tsteps*dt,tsteps)
+t_array = linspace(0.,(tsteps-1)*dt,tsteps)
 x_array = linspace(-5.,5.,meshsize)
 y_array = linspace(-5.,5.,meshsize)
 
 
+call dispersion_relation_array(dispersions)
+call amplitudes_array(amplitudes)
+    
 
-
+open(1,file='velocities.dat')
 
 
 do t_c = 1, tsteps
 
 t = t_array(t_c)
+
+call MaduLawrence_loop(time1, phase1, t, vu)
+call MaduLawrence_loop(time2, phase2, t, vu)
+    
 
 
 do x_c = 1, meshsize
@@ -50,8 +61,10 @@ do y_c = 1, meshsize
 
 y = y_array(y_c)
 
-vel = velocitynoML(x,y,t,time1,time2,phase1,phase2)
+vel = velocity_pointML(x_array(x_c),y_array(y_c),t,phase1,phase2,time1,time2,amplitudes,dispersions,g)
+        
 
+write(1,*) vel
 
 
 
@@ -60,13 +73,7 @@ end do
 end do
 end do
 
-
-
-
-
-
-
-end function velocitycomp
+end subroutine velocitycomp
 
 
 end program velocity test
