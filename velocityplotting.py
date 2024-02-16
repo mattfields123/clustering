45c870086d9f8886c90cdb980ca710fa0e61a716
bunny <kawaiiitzuyu@gmail.com>
2024-02-16 17:20:04 +0000

    velocity plotting works okay

--- velocityplotting.py
+++ velocityplotting.py
@@ -1,17 +1,41 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegWriter
 
-
-with open('velocities.dat') as file_name:
+with open('/Users/bunny/Documents/msci/mscigit/veltest/velocities.dat') as file_name:
     velocities = np.loadtxt(file_name)
 
 
 tsteps = 100
 meshsize = 10
 
-velocities.reshape((tsteps,meshsize))
+velocities = velocities.reshape(tsteps,meshsize,meshsize)
+
+
+print(velocities.shape)
 
 
 
+fig = plt.figure(figsize=(12, 12))
+axes = plt.subplot()
 
+
+def init_func():
+    plt.cla()
+
+
+def update_plot(ii):
+    plt.cla()
+    plt.xlabel('X (km)')
+    plt.ylabel('Y (km)')
+    plt.imshow(velocities[ii])
+
+
+anim = FuncAnimation(fig,
+                     update_plot,
+                     frames=np.arange(0, 100),
+                     init_func=init_func)
+
+writervideo = FFMpegWriter(fps=20)
+anim.save('mscigit/velocityxprofile.mp4', writer=writervideo)
 
