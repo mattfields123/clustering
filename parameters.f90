5a35a8ea855ab2f7dc9e9d3525bc2d08b70cbd5f
mf620 <mf620@ic.ac.uk>
2024-02-27 14:00:19 +0000

    yprofile

--- parameters.f90
+++ parameters.f90
@@ -6,7 +6,7 @@ real, parameter :: gamma = 1.
 real :: delta = 1.
 real(dp) :: vu = 10*atan(1.)
 real :: amp_scaling = 1.
-integer :: time_steps = 5
+integer :: time_steps = 500
 integer :: N_particles = 100
 
 end module parameters
