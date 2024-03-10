83df17cfcdadd0f0dc3f7d388281a74b807dfe93
mf620 <mf620@ic.ac.uk>
2024-03-10 14:29:56 +0000

    owo?

--- parameters.f90
+++ parameters.f90
@@ -2,9 +2,9 @@ module parameters
 use constants
         implicit none
 
-real, parameter :: gamma = 0.1
-real :: delta = 0.1
-real(dp) :: vu = 10*atan(1.)
+real, parameter :: gamma = 1.
+real :: delta = 1.
+real(dp) :: vu = 10000*atan(1.)
 real :: amp_scaling = 1.
 integer :: time_steps = 500
 integer :: N_particles = 100
