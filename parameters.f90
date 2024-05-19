a8d0fd45f49f16773b4cc1099505ff14ece59f18
mf620 <mf620@ic.ac.uk>
2024-05-19 23:41:11 +0100

    lawrence drift runniong

--- parameters.f90
+++ parameters.f90
@@ -3,10 +3,10 @@ use constants
         implicit none
 
 real, parameter :: gamma = 0.1
-real :: delta = 1.0
-real(dp) :: vu = 10*atan(1.)
+real :: delta = 0.1
+real(dp) :: vu = 10000*atan(1.)
 real :: amp_scaling = 1.
-integer :: time_steps = 500
-integer :: N_particles = 200
+integer :: time_steps = 1000
+integer :: N_particles = 500
 
 end module parameters
